7e2dd9877086dd3568ed8b47937d0a3dd22a5310
username-generation-failed <aspic.jellied.fish+gb@gmail.com>
2025-07-21 14:37:16 +0300

    refactor: add factory for AnimalsSearch into a separate file

--- rs-react-app1/src/features/Animals/AnimalsSearchList/AnimalsSearch.tsx
+++ rs-react-app1/src/features/Animals/AnimalsSearchList/AnimalsSearch.tsx
@@ -1,15 +1,7 @@
-import Search from '~components/Search';
 import { LocalStoragePersistor } from '~lib/Persistor';
-import { withPersist } from '~components/Persist';
+import { animalsSearchFactory } from './animalsSearchFactory';
 
-const AnimalsSearch = withPersist(
-  Search,
-  { defaultValue: '' },
-  {
-    onSearch: (data) => (value) => {
-      data.defaultValue = value;
-    },
-  },
+const AnimalsSearch = animalsSearchFactory(
   new LocalStoragePersistor('animals_search_state_persistor')
 );
 
