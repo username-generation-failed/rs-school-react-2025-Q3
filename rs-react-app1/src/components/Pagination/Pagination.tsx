e2026bd2fc94a5c9ea8ab540c22499a190525cbb
username-generation-failed <aspic.jellied.fish+gb@gmail.com>
2025-07-28 00:23:53 +0300

    fix: update control pages on pagination

--- rs-react-app1/src/components/Pagination/Pagination.tsx
+++ rs-react-app1/src/components/Pagination/Pagination.tsx
@@ -10,22 +10,43 @@ export type Props = {
 };
 const generate = (
   count: number,
+  offset: number,
   activePage: number,
   renderItem: Props['renderItem']
-) => [...Array(count).keys()].map((page) => renderItem(page + 1, activePage));
+) =>
+  [...Array(count).keys()].map((page) =>
+    renderItem(page + 1 + offset, activePage)
+  );
 export const Pagination = (props: Props) => {
   const { activePage, renderItem, lastPage, className } = props;
 
   if (lastPage <= 1) return null;
 
-  const items =
-    lastPage < 5
-      ? generate(lastPage, activePage, renderItem)
-      : [
-          ...generate(5, activePage, renderItem),
-          <PaginationEllipsis key="pagination_ellipsis" />,
-          renderItem(lastPage, activePage),
-        ];
+  let items: ReactNode[];
+
+  if (lastPage < 5) {
+    items = generate(lastPage, 0, activePage, renderItem);
+  } else if (activePage < 4) {
+    items = [
+      ...generate(5, 0, activePage, renderItem),
+      <PaginationEllipsis key="pagination_ellipsis" />,
+      renderItem(lastPage, activePage),
+    ];
+  } else if (lastPage - activePage > 2) {
+    items = [
+      renderItem(1, activePage),
+      <PaginationEllipsis key="pagination_ellipsis" />,
+      ...generate(3, activePage - 2, activePage, renderItem),
+      <PaginationEllipsis key="pagination_ellipsis" />,
+      renderItem(lastPage, activePage),
+    ];
+  } else {
+    items = [
+      renderItem(1, activePage),
+      <PaginationEllipsis key="pagination_ellipsis" />,
+      generate(5, lastPage - 5, activePage, renderItem),
+    ];
+  }
 
   return (
     <div className={clsx('flex justify-center gap-x-1', className)}>
